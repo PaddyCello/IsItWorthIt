f6327278859f7493a7e990aca0a74327b5cedad8
Hamza Butt <hamzambutt11@gmail.com>
2021-03-11 12:31:52 +0000

    Country card calls new API successfully

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
 
 const CountryCard = () => {
 
+  const [countryInfo, setCountriesInfo] = useState(null)
+  const params = useParams()
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get(`https://travelbriefing.org/${params.id}?format=json`)
+      setCountriesInfo(response)
+    }
+    getData()
+  }, [])
+  console.log(countryInfo.data.names.name)
   return (
-    <h1>country card ready!</h1>
+    <h1>{countryInfo.data.names.name}</h1>
   )
 }
 export default CountryCard
