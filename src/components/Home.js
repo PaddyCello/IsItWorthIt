787d16de698e695bb09bed46f735fc0bef2a728b
Patrick Johnson <paddyjohnsonmusic@gmail.com>
2021-03-12 10:16:24 +0000

    home page styled

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,10 +11,10 @@ const Home = () => {
 
   return ( 
     <div className='home'>
-      <p>Picture the scene: after months upon months of waiting, the world is vaccinated, Covid is vanquished, and you are ready and able to go on holiday again.  Desperate to leave your house, your street, your country, you buy the first airplane tickets you can get your hands on.  You don’t know where you’re flying to, and you don’t care - as long as you’re not in your living room with Joe Wicks on a five-inch screen and the pungent aroma of sourdough starter in the air, you’ll take it.</p>
-      <p>But before you board that plane, ask yourself one thing...</p>
-      <h1>IS IT WORTH IT?</h1>
-      <button onClick={handleClick}>Click here</button><p> before you do anything stupid.</p>
+      <p className='home-text'>Picture the scene: after months upon months of waiting, the world is vaccinated, Covid is vanquished, and you are ready and able to go on holiday again.  Desperate to leave your house, your street, your country, you buy the first airplane tickets you can get your hands on.  You don’t know where you’re flying to, and you don’t care - as long as you’re not in your living room with Joe Wicks on a five-inch screen and the pungent aroma of sourdough starter in the air, you’ll take it.</p>
+      <p className='home-text'>But before you board that plane, ask yourself one thing...</p>
+      <h1 className='home-big'>IS IT WORTH IT?</h1>
+      <button onClick={handleClick}>Click here</button><p className='home-text'> before you do anything stupid.</p>
     </div>
   )
 
